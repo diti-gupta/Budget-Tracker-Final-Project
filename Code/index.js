e1535feddd11c0d9c2c15f90cefc279184b6e97a
diti-gupta <digu6278@colorado.edu>
2023-11-13 11:19:58 -0700

    adding positive test case

--- Code/index.js
+++ Code/index.js
@@ -4,7 +4,9 @@
   const pgp = require("pg-promise")();
   const bodyParser = require("body-parser");
   const session = require("express-session");
-  
+  const bcrypt = require('bcrypt'); //  To hash passwords
+  const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
+
   // db config
   const dbConfig = {
     host: "db",
@@ -49,6 +51,60 @@
   app.get('/welcome', (req, res) => {
     res.json({status: 'success', message: 'Welcome!'});
   });
+
+  
+  app.post('/login', async (req,res)=>{
+    try {
+      const { username, password } = req.body;
+      
+      //DG
+      console.log("In app.post login");
+      console.log(username, password);
+
+      // Find the user from the users table
+      const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', username);
+      if (!user) {
+        // If the user is not found, redirect to the registration page
+        console.log("In ! user");
+        return res.redirect('/register');
+      }
+  
+      // Use bcrypt.compare to check if the entered password matches the hashed password in the database
+      console.log("user.password length:", user.password);
+      console.log("actual password length", password);
+      const passwordMatch = (password == user.password);
+
+      console.log("DOES password MATCH?", passwordMatch);
+      if (!passwordMatch) {
+        // If the password is incorrect, throw an error and redirect to login
+        console.log("passwords don't match!")
+        throw new Error('Incorrect username or password');
+      }
+  
+      // Save the user in the session
+      req.session.user = user;
+      console.log("username & password validated!!");
+      req.session.save(() => {
+        // Redirect to the /register for now TBD route after setting the session
+        res.redirect('/register');
+      });
+    } catch (error) {
+      // If the database request fails or if there is an incorrect password, handle the error
+      res.render('pages/login', { error: error.message }); // Render the login page with an error message
+    }
+    // Authentication Middleware.
+  const auth = (req, res, next) => {
+    if (!req.session.user) {
+      // Default to login page.
+      return res.redirect('/login');
+    }
+    next();
+  };
+  
+  // Authentication Required
+  app.use(auth);
+});
+
 
 module.exports = app.listen(3000);
 console.log("Server is listening on port 3000");
