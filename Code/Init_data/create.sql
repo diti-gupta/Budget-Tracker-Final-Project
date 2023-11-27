bcdf75a8eff116d4f5e092e288b20e0794b5e34b
diti-gupta <digu6278@colorado.edu>
2023-11-26 22:48:20 -0700

    updating insert.sql

--- Code/Init_data/create.sql
+++ Code/Init_data/create.sql
@@ -1,6 +1,35 @@
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-    password CHAR(60) NOT NULL,
-    username VARCHAR(50) PRIMARY KEY
+CREATE TABLE Users (
+    Username VARCHAR(60) PRIMARY KEY,
+    Password VARCHAR(60) NOT NULL
+);
+/*User Budget Table*/
+CREATE TABLE User_Budget (
+    Budget_id INT PRIMARY KEY,
+    Month INT NOT NULL
 );
 
+/*Users TO Budget Table*/
+CREATE TABLE Users_to_Budget (
+    Username VARCHAR(60),
+    Budget_id INT,
+    FOREIGN KEY (Username) REFERENCES Users(Username),
+    FOREIGN KEY (Budget_id) REFERENCES User_Budget(Budget_id)
+);
+
+/*Income Expense Table*/
+CREATE TABLE Income_Expense (
+    Index_ID INT PRIMARY KEY,
+    Category VARCHAR(50),
+    Amount FLOAT NOT NULL,
+    Total FLOAT NOT NULL,
+    Label VARCHAR(50)
+);
+
+/*Budget TO Income Table*/
+CREATE TABLE Budget_to_Income (
+    Budget_ID INT,
+    Index_ID INT,
+    FOREIGN KEY (Budget_id) REFERENCES User_Budget(Budget_id),
+    FOREIGN KEY (Index_ID) REFERENCES Income_Expense(Index_ID)
+);
