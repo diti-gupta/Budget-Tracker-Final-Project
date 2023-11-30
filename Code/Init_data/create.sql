1248507a074fe802a45de0562802e040c37785f3
Adam Fox <adfo6015@colorado.edu>
2023-11-30 13:35:07 -0700

    updated db

--- Code/Init_data/create.sql
+++ Code/Init_data/create.sql
@@ -3,21 +3,8 @@ CREATE TYPE CategoryEnum AS ENUM ('Salary', 'Investments', 'Transportation', 'Fo
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE Users (
     Username VARCHAR(60) PRIMARY KEY,
+    Budget_ID NOT NULL INT,
     Password VARCHAR(60) NOT NULL
-);
-
-/*User Budget Table*/
-CREATE TABLE User_Budget (
-    Budget_id INT PRIMARY KEY,
-    Current_month INT NOT NULL
-);
-
-/*Users TO Budget Table*/
-CREATE TABLE Users_to_Budget (
-    Username VARCHAR(60),
-    Budget_id INT,
-    FOREIGN KEY (Username) REFERENCES Users(Username),
-    FOREIGN KEY (Budget_id) REFERENCES User_Budget(Budget_id)
 );
 
 /*Income Expense Table*/
@@ -27,13 +14,16 @@ CREATE TABLE Income_Expense (
     Category CategoryEnum,
     Amount FLOAT NOT NULL,
     Total FLOAT NOT NULL,
+    Monthh INT,
     Label VARCHAR(50)
 );
 
 /*Budget TO Income Table*/
-CREATE TABLE Budget_to_Income (
+CREATE TABLE Users_to_Income (
     Budget_ID INT,
     Index_ID INT,
-    FOREIGN KEY (Budget_id) REFERENCES User_Budget(Budget_id),
-    FOREIGN KEY (Index_ID) REFERENCES Income_Expense(Index_ID)
+    Monthh INT,
+    FOREIGN KEY (Budget_id) REFERENCES Users(Budget_id),
+    FOREIGN KEY (Index_ID) REFERENCES Income_Expense(Index_ID),
+    FOREIGN KEY (Monthh) REFERENCES Income_Expense(Monthh),
 );
