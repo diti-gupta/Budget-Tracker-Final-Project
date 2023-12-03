a9efca835d46019f1d6a508401fc1a999f0b29e8
diti-gupta <digu6278@colorado.edu>
2023-12-03 02:05:52 -0700

    adding changes to Diti Branch for pie chart

--- Code/Init_data/insert.sql
+++ Code/Init_data/insert.sql
@@ -1,19 +1,19 @@
 INSERT INTO users (password, username) VALUES
     ('$2b$10$AOC7paRUgmHE.lZe1yTlmeVNRA5bTFbMNI35CtTg767QzA1ngVVEW', 'user8');
 
--- Please note that the index_id will automatically be updated because the Income_Expense Index_ID is of type SERIAL 
-INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
-VALUES ('user8','Salary', 4000.00, 2, 'January Salary'); 
+-- -- Please note that the index_id will automatically be updated because the Income_Expense Index_ID is of type SERIAL 
+-- INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
+-- VALUES ('user8','Salary', 4000.00, 2, 'January Salary'); 
 
 
-INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
-VALUES ('user8','Investments', 1000.00, 1, 'January Investment');
+-- INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
+-- VALUES ('user8','Investments', 1000.00, 1, 'January Investment');
 
-INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
-VALUES ('user8','Salary', 5000.00, 2, 'February Salary - Increased');
+-- INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
+-- VALUES ('user8','Salary', 5000.00, 2, 'February Salary - Increased');
 
-INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
-VALUES ('user8','Investments', 1000.00, 2, 'February Investment');
+-- INSERT INTO Income_Expense (Username, Category, Amount, Monthh, Label)
+-- VALUES ('user8','Investments', 1000.00, 2, 'February Investment');
 
 
 -- INSERT INTO Users_to_Income (Username, Index_ID, Monthh)
